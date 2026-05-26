f7cdb6c2ee3b09699e479ffb313ec6c79b14651f
Rui Patinha <rui.patinha@gmail.com>
2026-05-26 09:54:22 +0100

    Support both Apple Silicon and Intel macOS

--- Formula/bkt.rb
+++ Formula/bkt.rb
@@ -1,10 +1,20 @@
 class Bkt < Formula
   desc "Tiny glab-style CLI for Bitbucket Cloud"
   homepage "https://github.com/rfp/bkt"
-  url "https://github.com/rfp/bkt/releases/download/v0.1.0/bkt_0.1.0_darwin_arm64.tar.gz"
-  sha256 "ed28d1acd79825aaabcbf31d03bfe1dd3694430f43b44498698778c2df53699c"
   license "MIT"
   version "0.1.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/rfp/bkt/releases/download/v0.1.0/bkt_0.1.0_darwin_arm64.tar.gz"
+      sha256 "ed28d1acd79825aaabcbf31d03bfe1dd3694430f43b44498698778c2df53699c"
+    end
+
+    on_intel do
+      url "https://github.com/rfp/bkt/releases/download/v0.1.0/bkt_0.1.0_darwin_amd64.tar.gz"
+      sha256 "ca513a6ae2d3abfc0e6ae9b2f7138e6576aef168b82b87017675ab16d8a258f8"
+    end
+  end
 
   depends_on :macos
 
@@ -13,6 +23,6 @@ class Bkt < Formula
   end
 
   test do
-    assert_match "bkt version", shell_output("#{bin}/bkt version")
+    assert_match "bkt version 0.1.0", shell_output("#{bin}/bkt version")
   end
 end
